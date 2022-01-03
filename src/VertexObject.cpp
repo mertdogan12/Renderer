b2f30257fb9d3ef512cf354efb017141b83ce921
Mert Dogan <mert.dogan@me.com>
2022-01-04 00:01:53 +0100

    VertexObject created

---
+++ src/VertexObject.cpp
@@ -0,0 +1,59 @@
+#include "VertexObject.h"
+
+#include "glm/glm.hpp"
+
+namespace renderer {
+    VertexObject::VertexObject(const std::string name, const float coords[2], 
+            const float width, const float height, const std::string& texturePath)
+        : Name(name), VertexTexture(texturePath)
+    {
+        Vertex v0 = 
+        {
+            glm::vec3(coords[0], coords[1], 0.0f),
+            glm::vec4(0.0f, 0.0f, 0.0f, 1.0f),
+            glm::vec2(0.0f, 0.0f),
+            0.0f
+        };
+
+        Vertex v1 = 
+        {
+            glm::vec3(coords[0] + width, coords[1], 0.0f),
+            glm::vec4(0.0f, 0.0f, 0.0f, 1.0f),
+            glm::vec2(1.0f, 0.0f),
+            0.0f
+        };
+
+        Vertex v2 = 
+        {
+            glm::vec3(coords[0] + width, coords[1] + height, 0.0f),
+            glm::vec4(0.0f, 0.0f, 0.0f, 1.0f),
+            glm::vec2(1.0f, 1.0f),
+            0.0f
+        };
+
+        Vertex v3 = 
+        {
+            glm::vec3(coords[0], coords[1] + height, 0.0f),
+            glm::vec4(0.0f, 0.0f, 0.0f, 1.0f),
+            glm::vec2(0.0f, 1.0f),
+            0.0f
+        };
+
+        Vertexs[0] = &v0;
+        Vertexs[1] = &v1;
+        Vertexs[2] = &v2;
+        Vertexs[2] = &v3;
+    }
+
+    void VertexObject::BindTexture(unsigned int slot)
+    {
+        setTextureIndex((float) slot);
+        VertexTexture.Bind(slot);
+    }
+
+    void VertexObject::setTextureIndex(float index) const
+    {
+        for (int i = 0; i < 4; i++)
+            Vertexs[i]->TextureIndex = index;
+    }
+}
---
+++ src/VertexObject.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Texture.h"
+
+#include "string"
+#include "memory"
+
+#include "glm/glm.hpp"
+
+namespace renderer {
+    struct Vertex 
+    {
+        glm::vec3 Coords;
+        glm::vec4 Rgba;
+        glm::vec2 TexCoords;
+        float TextureIndex;
+    };
+
+    class VertexObject
+    {
+        public:
+            VertexObject(const std::string name, const float coords[2], 
+                    const float width, const float height, const std::string& texturePath);
+
+            std::string Name;
+            Vertex* Vertexs[4];
+
+            Texture VertexTexture;
+            void BindTexture(unsigned int slot = 0);
+
+        private:
+            void setTextureIndex(float index) const;
+    };
+}
