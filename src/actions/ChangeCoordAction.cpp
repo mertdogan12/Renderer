512417eb74a5357b28b1738d03a1d891ac6773a1
Mert Dogan <mert.dogan@me.com>
2022-10-13 03:01:39 +0200

    init update

--- src/actions/ChangeCoordAction.cpp
+++ src/actions/ChangeCoordAction.cpp
@@ -1,12 +1,35 @@
 #include "actions/ChangeCoordAction.h"
 
+#include "data/Helper.h"
+#include "renderer/Renderer.h"
+#include "renderer/VertexObject.h"
+
 namespace actions {
     ChangeCoordsAction::ChangeCoordsAction(Time time, char textureId) 
         : Action(time, textureId) {}
 
     void ChangeCoordsAction::init(const char *args)
     {
+        // Parses the args
+        endPos.x = data::charsToFloat(args);
+        endPos.y = data::charsToFloat(args, 4 * 1);
+        endPos.scaleX = data::charsToFloat(args, 4 * 2);
+        endPos.scaleY = data::charsToFloat(args, 4 * 3);
 
+        // Gets the current Position from the Texture and sets it to the start Position
+        renderer::VertexObject* obj = renderer::Renderer::map[0];
+
+        Position startPos = 
+        {
+            obj->posX, 
+            obj->posY, 
+            obj->posScaleX, 
+            obj->posScaleY
+        };
+
+        // Calcuates the speed
+        speed.x = (endPos.x - startPos.x) / (time.end - time.currentPos);
+        speed.y = (endPos.y - startPos.y) / (time.end - time.currentPos);
     }
 
     void ChangeCoordsAction::run() 
--- src/actions/ChangeCoordAction.h
+++ src/actions/ChangeCoordAction.h
@@ -5,9 +5,28 @@
 namespace actions {
     class ChangeCoordsAction: public Action
     {
+        // How much x or y should be increased
+        struct Speed {
+            int x;
+            int y;
+        };
+
+        struct Position {
+            float x;
+            float y;
+            float scaleX;
+            float scaleY;
+        };
+
         public:
             ChangeCoordsAction(Time time, char textureId);
             void init(const char *args) override;
             void run() override;
+
+        private:
+            Speed speed;
+
+            // end coordinates
+            Position endPos;
     };
 }
